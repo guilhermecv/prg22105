20371bb0f6158678153084cfc9c2a53e0acc9fdf
guilherme.valese@hotmail.com <guilherme.valese@hotmail.com>
2019-11-21 19:15:05 -0300

    insertion sort direto adicionado

--- Ordenacao/insertion_sort_direto/main.c
+++ Ordenacao/insertion_sort_direto/main.c
@@ -3,23 +3,18 @@
  *
  * @author Guilherme Camargo Valese
  *
- * @date 29/10/2019
- *
- * @description Algoritmo de Ordenação por Bubble Sort
+ * @description Algoritmo de Ordenação por Inserção Direta
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "dados.h"
 #include <time.h>
 #include <limits.h>
+#include "dados.h"
+#include "insertion_sort.h"
 
 #define DEBUG_ON
 #define ITERACOES     20
-
-void bubble_sort(dado_t **dados, int *n);
-void swap(dado_t **a , dado_t **b);
-
 
 int main()
 {
@@ -36,7 +31,7 @@ int main()
       dado_t **dados = ler_dados_csv("camera_temp.csv", &n_linhas);
 
       clock_gettime(CLOCK_MONOTONIC ,&t_1);
-      bubble_sort(dados, &n_linhas);
+      insertion_sort(dados, &n_linhas);
       clock_gettime(CLOCK_MONOTONIC ,&t_2);
 
       total_time.tv_sec += (t_2.tv_sec - t_1.tv_sec);
@@ -56,44 +51,4 @@ int main()
     printf("\nTeste finalizado com %d iteracoes\n\n\tTempo medio de execucao: \n\t%lu s \n\t%lu ns\n\n", i, total_time.tv_sec/ITERACOES, total_time.tv_nsec/ITERACOES);
 
     return EXIT_SUCCESS;
-}
-
-
-/**
- * @brief Ordenação por bubble sort
- * @param **dados: vetor de ponteiros
- * @param n: quantidade total de elementos
- */
-void bubble_sort(dado_t **dados, int *n)
-{
-    if(dados == NULL)
-    {
-        fprintf(stderr, "bubble_sort(): ponteiros invalidos\n");
-        exit(EXIT_FAILURE);
-    }
-
-    int i,j;
-    for (i = *n; i > 1; i--)
-    {
-        for(j = 0; j < i - 1; j++)
-        {
-            if(dados[j]->temperatura > dados[j+1]->temperatura)
-            {
-                swap(&dados[j], &dados[j+1]);
-            }
-        }
-    }
-}
-
-/**
- * @brief Faz a troca de dois elementos de um vetor
- * @param a: ponteiro para o primeiro elemento
- * @param b: ponteiro para o segundo elemento
- */
-void swap(dado_t **a , dado_t **b)
-{
-    dado_t *temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
 }
