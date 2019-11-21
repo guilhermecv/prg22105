5c1c2f740e1b8314dc4bf9c0cb7c70cd541f00bc
guilherme.valese@hotmail.com <guilherme.valese@hotmail.com>
2019-11-21 19:20:02 -0300

    bubble sort atualizado

--- Ordenacao/bubble_sort/main.c
+++ Ordenacao/bubble_sort/main.c
@@ -10,16 +10,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "dados.h"
 #include <time.h>
 #include <limits.h>
+#include "dados.h"
+#include "bubble_sort.h"
 
 #define DEBUG_ON
 #define ITERACOES     20
-
-void bubble_sort(dado_t **dados, int *n);
-void swap(dado_t **a , dado_t **b);
-
 
 int main()
 {
@@ -56,44 +53,4 @@ int main()
     printf("\nTeste finalizado com %d iteracoes\n\n\tTempo medio de execucao: \n\t%lu s \n\t%lu ns\n\n", i, total_time.tv_sec/ITERACOES, total_time.tv_nsec/ITERACOES);
 
     return EXIT_SUCCESS;
-}
-
-
-/**
- * @brief Ordenação por bubble sort
- * @param **dados: vetor de ponteiros
- * @param n: quantidade total de elementos
- */
-void bubble_sort(dado_t **dados, int *n)
-{
-    if(dados == NULL)
-    {
-        fprintf(stderr, "bubble_sort(): ponteiros invalidos\n");
-        exit(EXIT_FAILURE);
-    }
-
-    int i,j;
-    for (i = *n; i > 1; i--)
-    {
-        for(j = 0; j < i - 1; j++)
-        {
-            if(dados[j]->temperatura > dados[j+1]->temperatura)
-            {
-                swap(&dados[j], &dados[j+1]);
-            }
-        }
-    }
-}
-
-/**
- * @brief Faz a troca de dois elementos de um vetor
- * @param a: ponteiro para o primeiro elemento
- * @param b: ponteiro para o segundo elemento
- */
-void swap(dado_t **a , dado_t **b)
-{
-    dado_t *temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
 }
