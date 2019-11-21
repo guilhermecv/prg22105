028cd6331bdcbc2be5a40d46a3b3c7e5389799b2
guilherme.valese@hotmail.com <guilherme.valese@hotmail.com>
2019-11-21 19:02:31 -0300

    quick sort funcionando

--- Ordenacao/quick_sort/main.c
+++ Ordenacao/quick_sort/main.c
@@ -7,17 +7,12 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include "dados.h"
 #include <time.h>
 #include <limits.h>
+#include "dados.h"
+#include "quick_sort.h"
 
-// #define DEBUG
 #define ITERACOES		20
-
-
-void swap(dado_t **a , dado_t **b);
-void quick_sort(dado_t **dados, int esq, int dir);
-
 
 int main()
 {
@@ -53,56 +48,4 @@ int main()
     printf("\nTeste finalizado com %d iteracoes\n\n\tTempo de execucao: \n\t%lu s \n\t%lu ns\n\n", i, total_time.tv_sec/ITERACOES, total_time.tv_nsec/ITERACOES);
 
     return EXIT_SUCCESS;
-}
-
-/**
- * @brief
- */
-void quick_sort(dado_t **dados, int esq, int dir)
-{
-#ifdef DEBUG
-  printf("\nesq: %d, dir: %d\n", esq, dir);
-#endif
-
-  int i, j;
-  i = esq;
-  j = dir;
-
-  int med = (esq + dir) / 2;
-
-  float pivo = dados[med]->temperatura;
-
-#ifdef DEBUG
-  printf("\nmediana: %d, pivo selecionado: %f", med, pivo);
-#endif
-
-  do
-  {
-    while(dados[i]->temperatura < pivo) i++;
-    while(dados[j]->temperatura > pivo) j--;
-
-    if(i <= j)
-    {
-      swap(&dados[i], &dados[j]);
-      i++;
-      j--;
-    }
-  } while(j > i);
-
-  if(esq < j) quick_sort(dados, esq, j);
-  if(i < dir) quick_sort(dados, i, dir);
-}
-
-
-/**
- * @brief Faz a troca de dois elementos de um vetor
- * @param a: ponteiro para o primeiro elemento
- * @param b: ponteiro para o segundo elemento
- */
-void swap(dado_t **a , dado_t **b)
-{
-    dado_t *temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
 }
